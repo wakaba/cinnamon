7d15999feb2354e3aa6f726997f5d7d84cf3ea75
Wakaba <wakabatan@hatena.ne.jp>
2012-10-17 11:27:45 +0900

    checkout a specific revision

--- lib/Cinnamon/Task/Git.pm
+++ lib/Cinnamon/Task/Git.pm
@@ -32,7 +32,7 @@ task git => {
             my $branch = (get 'git_branch') || 'master';
             $branch =~ s{^origin/}{};
             $result->{old_revision} = get_git_revision; # or undef
-            run_stream "git clone $url $dir || true";
+            run_stream "git clone $url $dir || (cd $dir && git fetch)";
             run_stream "cd $dir && (git checkout $branch || git checkout -b $branch origin/$branch) && git pull";
             run_stream "cd $dir && git submodule update --init";
             $result->{new_revision} = get_git_revision; # or undef
@@ -42,6 +42,38 @@ task git => {
             log error => sprintf "Remote revision is %s (local is %s)",
                 (substr $result->{new_revision}, 0, 10),
                 (substr $local_rev || '', 0, 10);
+            $result->{error}->{revision_mismatch} = 1;
+        }
+
+        return $result;
+    },
+    checkout => sub {
+        my ($host, $rev, @args) = @_;
+        my $result = {};
+        die "Usage: checkout /revision/\n" unless $rev;
+        my $exact = $rev =~ s/^=//;
+        remote {
+            my $dir = (get 'git_deploy_dir') || (get 'deploy_dir');
+            my $url = get 'git_repository';
+            $result->{old_revision} = get_git_revision; # or undef
+            if ($result->{old_revision} and not $exact) {
+                my ($newer) = run "(cd \Q$dir\E && git rev-list HEAD | grep \Q$rev\E > /dev/null && echo 1) || echo 0";
+                chomp $newer;
+                if ($newer) {
+                    warn "Remote commit ($result->{old_revision}) is newer; skipped\n";
+                }
+            }
+            run_stream "git clone $url $dir || (cd $dir && git fetch)";
+            run_stream "cd $dir && git checkout \Q$rev\E";
+            run_stream "cd $dir && git submodule update --init";
+            $result->{new_revision} = get_git_revision; # or undef
+        } $host, user => get 'git_clone_user';
+
+        if ($result->{new_revision} ne $rev and
+            $result->{new_revision} !~ /^\Q$rev\E/) {
+            log error => sprintf "Remote revision is %s (expected is %s)",
+                (substr $result->{new_revision}, 0, 10),
+                (substr $rev || '', 0, 10);
             $result->{error}->{revision_mismatch} = 1;
         }
 
