54352f76111c29e00e998e24e5e3214b4c6cb5d3
Wakaba <wakabatan@hatena.ne.jp>
2012-11-11 19:58:17 +0900

    added scheduling method

--- lib/Cinnamon/Task/AutoTest2013.pm
+++ lib/Cinnamon/Task/AutoTest2013.pm
@@ -4,11 +4,34 @@ use warnings;
 use Cinnamon::Task::HTTP;
 use Cinnamon::DSL;
 use AnyEvent;
+use Exporter::Lite;
+
+our @EXPORT = qw(schedule_test);
+
+sub schedule_test ($$$) {
+    my ($repo, $branch, $rev) = @_;
+    my $test_host = get 'autotest_host';
+    my $cv = AE::cv;
+    http_post_json
+        url => qq<http://$test_host/jobs>,
+        basic_auth => [api_key => get 'autotest_api_key'],
+        content => {
+            repository => {url => $repo},
+            ref => (defined $branch ? 'refs/heads/' . $branch : undef),
+            after => $rev,
+        },
+        anyevent => 1,
+        cb => sub {
+            my (undef, $res) = @_;
+            die "Failed to insert test job\n" if $res->is_error;
+            $cv->send;
+        };
+    $cv->recv;
+}
 
 task autotest2013 => {
     schedule => sub {
         my ($host, @args) = @_;
-        my $test_host = get 'autotest_host';
         my $rev = `git rev-parse HEAD` or die "Can't get git commit\n";
         chomp $rev;
         my $branch = `git branch`;
@@ -18,22 +41,8 @@ task autotest2013 => {
         } else {
             undef $branch;
         }
-        my $cv = AE::cv;
-        http_post_json
-            url => qq<http://$test_host/jobs>,
-            basic_auth => [api_key => get 'autotest_api_key'],
-            content => {
-                repository => {url => get 'git_repository'},
-                ref => (defined $branch ? 'refs/heads/' . $branch : undef),
-                after => $rev,
-            },
-            anyevent => 1,
-            cb => sub {
-                my (undef, $res) = @_;
-                die "Failed to insert test job\n" if $res->is_error;
-                $cv->send;
-            };
-        $cv->recv;
+        my $repo = get 'git_repository';
+        schedule_test $repo, $branch, $rev;
     },
 };
 
