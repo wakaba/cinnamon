60f40f400b945dbfa507abbe1c927734c1d23544
Wakaba <wakabatan@hatena.ne.jp>
2013-07-01 18:50:35 +0900

    Revert "Merge commit 'cbfc13cb224872eb320852db50a410333c53ccae'"
    This reverts commit ccff0e716c92e1859fa0121184b38d1f63178027, reversing
    changes made to f041e0e53060f3b3bf29a9dcc92f050ea48852eb.

--- lib/Cinnamon/Runner.pm
+++ lib/Cinnamon/Runner.pm
@@ -5,51 +5,29 @@ use warnings;
 use Cinnamon::Logger;
 use Cinnamon::Config;
 
-use Coro;
-use Coro::Select;
-
 sub start {
     my ($class, $hosts, $task, @args) = @_;
-    my $all_results = {};
-    $hosts = [ @$hosts ];
 
-    my $task_name           = Cinnamon::Config::get('task');
-    my $concurrency_setting = Cinnamon::Config::get('max_concurrency') || {};
-    my $concurrency         = $concurrency_setting->{$task_name} || scalar @$hosts;
-
-    while (my @target_hosts = splice @$hosts, 0, $concurrency) {
-        my @coros;
+    my %result;
+    for my $host (@$hosts) {
+        $result{$host} = +{ error => 0 };
 
         local $Cinnamon::Runner::Host = $host; # XXX AE unsafe
-        for my $host (@target_hosts) {
-            my $coro = async {
-                my $result = $class->execute($host, $task, @args);
-                $all_results->{$host} = $result;
-            };
+        eval { $task->($host, @args) };
 
-            push @coros, $coro;
+        if ($@) {
+            chomp $@;
+            log error => sprintf '[%s] %s', $host, $@;
+            $result{$host}->{error}++ ;
         }
-
-        $_->join for @coros;
     }
 
-    return $all_results;
+    \%result;
 }
 
 sub execute {
     my ($class, $host, $task, @args) = @_;
-
-    my $result = { host => $host, error => 0 };
-
-    local $@;
-    eval { $task->($host, @args) };
-    if ($@) {
-        chomp $@;
-        log error => sprintf '[%s] %s', $host, $@;
-        $result->{error} = 1;
-    }
-
-    return $result;
+    $task->($host, @args);
 }
 
-1;
+!!1;
