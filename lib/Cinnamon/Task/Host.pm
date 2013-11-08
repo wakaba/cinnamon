b128db86b05e915b653700fcf1e4844d3ae86adc
Wakaba <wakabatan@hatena.ne.jp>
2013-11-08 15:23:20 +0900

    concurrent host:name task

--- lib/Cinnamon/Task/Host.pm
+++ lib/Cinnamon/Task/Host.pm
@@ -4,12 +4,17 @@ use warnings;
 use Cinnamon::DSL;
 
 task host => {
-    name => sub {
-        my ($host, @args) = @_;
-        remote {
-            run q<hostname>;
-        } $host;
-    },
+    name => (taskdef {
+        my $state = shift;
+        my $cv = $state->create_result_cv;
+        for my $host (@{$state->hosts}) {
+            $cv->begin_host($host);
+            $state->remote(host => $host)->run_as_cv('hostname')->cb(sub {
+                $cv->end_host($host, $_[0]->recv);
+            });
+        }
+        return $cv->return;
+    } {hosts => 'all'}),
 };
 
 !!1;
