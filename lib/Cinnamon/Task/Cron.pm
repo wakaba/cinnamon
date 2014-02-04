df2cb8a41de38d97af561214d290229d75d802bb
Wakaba <wakabatan@hatena.ne.jp>
2014-02-04 14:35:53 +0900

    Added template for cron; Added descs to cron tasks

--- lib/Cinnamon/Task/Cron.pm
+++ lib/Cinnamon/Task/Cron.pm
@@ -3,38 +3,50 @@ use strict;
 use warnings;
 use Cinnamon::DSL;
 
-task cron => {
-    list => sub {
-        my ($host, @args) = @_;
-        remote {
-            sudo q<sh -c 'echo; for file in $(ls /etc/cron.d/); do echo "# $file"; cat "/etc/cron.d/$file"; echo; done'>;
-        } $host, user => get 'cron_user';
-    },
-    install => sub {
-        my ($host, @args) = @_;
-        remote {
-            my $dir = get 'deploy_dir';
-            sudo "cp -v $dir/local/config/cron.d/* /etc/cron.d/";
-            sudo 'chown -R root:root /etc/cron.d/';
-            sudo 'chmod -R 0644 /etc/cron.d/';
-            sudo 'chmod 0700 /etc/cron.d/';
-        } $host, user => get 'cron_user';
-    },
-    reload => sub {
-        my ($host, @args) = @_;
-        my $crond = get('cron_init') || '/etc/init.d/crond';
-        remote {
-            sudo "$crond reload";
-        } $host, user => get 'cron_user';
-    },
-    log => {
-        tail => sub {
-            my ($host, @args) = @_;
-            remote {
-                sudo_stream 'tail -f /var/log/cron';
-            } $host, user => get 'cron_user';
-        },
-    },
-};
+task cron => undef, {desc => 'cron'};
+
+task ['cron', 'list'] => sub {
+    my ($host, @args) = @_;
+    remote {
+        sudo q<sh -c 'echo; for file in $(ls /etc/cron.d/); do echo "# $file"; cat "/etc/cron.d/$file"; echo; done'>;
+    } $host, user => get 'cron_user';
+}, {desc => 'Show currently installed cron schedules'};
+
+task ['cron', 'install'] => sub {
+    my ($host, @args) = @_;
+    remote {
+        my $dir = get 'deploy_dir';
+        sudo "cp -v $dir/local/config/cron.d/* /etc/cron.d/";
+        sudo 'chown -R root:root /etc/cron.d/';
+        sudo 'chmod -R 0644 /etc/cron.d/';
+        sudo 'chmod 0700 /etc/cron.d/';
+    } $host, user => get 'cron_user';
+}, {desc => 'Install cron schedules'};
+
+task ['cron', 'reload'] => sub {
+    my ($host, @args) = @_;
+    my $crond = get('cron_init') || '/etc/init.d/crond';
+    remote {
+        sudo "$crond reload";
+    } $host, user => get 'cron_user';
+}, {desc => 'Let cron reload configurations'};
+
+task ['cron', 'log'] => undef, {desc => 'Logs'};
+
+task ['cron', 'log', 'tail'] => sub {
+    my ($host, @args) = @_;
+    remote {
+        sudo_stream 'tail -f /var/log/cron';
+    } $host, user => get 'cron_user';
+}, {desc => 'Tail cron log'};
+
+task ['cron', 'config'] => undef, {desc => 'Cron configurations'};
+
+task ['cron', 'config', 'create_file'] => sub {
+    my ($host, $name, @args) = @_;
+    $name = 'cron' unless defined $name and length $name;
+    run 'mkdir', '-p', 'config/cron.d';
+    run 'touch', 'config/cron.d/' . $name;
+}, {desc => '(local) Create schedule file placeholder'};
 
 !!1;
